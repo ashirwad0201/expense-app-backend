03397f7f54d19d2471c1492b3d76562134176769
ashirwad0201 <ashirwad0201@gmail.com>
2024-02-18 11:40:59 +0530

    password encryption

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,24 @@
 const Userdetail=require('../models/userdetail');
+const bcrypt=require('bcrypt');
 
 exports.insertUser = (req, res, next) => {
-   var myObj=req.body;
-   console.log(myObj)
-   console.log('hi')
-    
-    Userdetail.create(myObj)
-    .then(result=>{
-      console.log('user created');
-    //   res.redirect('/get-user')
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  var myObj=req.body;
+  console.log(myObj)
+  console.log('hi')
+  
+  const saltrounds=10;
+  bcrypt.hash(myObj.password,saltrounds, (err,hash)=>{
+    if(!err){
+      myObj.password=hash;
+      Userdetail.create(myObj)
+      .then(result=>{
+        console.log('user created');
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  })
   };
 
 exports.getUser=(req,res,next)=>{
@@ -49,20 +55,26 @@ exports.loginUser = (req, res, next) => {
       if(result==undefined){
         res.status(404);
         response="User doesn't exist";
+        return res.json(response)
       }
       else{
           var pass=result.password;
-          if(pass===myObj.password){
-            res.status(200);
-            response="Logged in successfully";
-          }
-          else{
-            res.status(401)
+          bcrypt.compare(myObj.password,pass,(err,result)=>{
+            if(err){
+              res.status(500);
+              response="Something went wrong";
+            }
+            if(result===true){
+              res.status(200);
+              response="Logged in successfully";
+            }
+            else{
+              res.status(401)
               response="Password incorrect!";
-          }
+            }
+            return res.json(response)
+          })
       }      
-      console.log(response)
-      res.json(response)
   })
   .catch(err=>console.log(err))
  };
