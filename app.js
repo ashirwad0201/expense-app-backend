99e54917aaf8196f6349fdf361e0cde06181bf1c
ashirwad0201 <ashirwad0201@gmail.com>
2024-02-20 19:52:59 +0530

    reset-password

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const adminRoutes = require('./routes/admin');
 const expenseRoutes = require('./routes/expense');
 const purchaseRoutes = require('./routes/purchase');
 const premiumRoutes = require('./routes/premium');
+const resetRoutes = require('./routes/reset');
 const UserDetail = require('./models/userdetail');
 const Expense = require('./models/expense')
 const Order=require('./models/order')
+const FPR=require('./models/forgotPasswordRequest')
 const app = express();
 app.use(cors());
 
@@ -19,12 +21,16 @@ app.use(adminRoutes);
 app.use(expenseRoutes);
 app.use(purchaseRoutes);
 app.use('/premium',premiumRoutes);
+app.use('/password',resetRoutes);
 
 UserDetail.hasMany(Expense);
 Expense.belongsTo(UserDetail);
 
 UserDetail.hasMany(Order);
 Order.belongsTo(UserDetail);
+
+UserDetail.hasMany(FPR);
+FPR.belongsTo(UserDetail);
 
 //sequelize.sync({force:true})
 sequelize.sync()
@@ -33,3 +39,4 @@ sequelize.sync()
     app.listen(5000);
 })
 .catch(err=>console.log(err));
+
